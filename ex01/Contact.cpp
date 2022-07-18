c7fff2ff65183fa61b1aacd0209bd8a2366ef04e
Fathiyat olatokunbo Jamia <fathjami@e3r12p13.1337.ma>
2022-07-18 20:54:16 +0100

    :100: learning and manipulating strings

---
+++ ex01/Contact.cpp
@@ -0,0 +1,19 @@
+#include "Contact.hpp"
+#include <iostream>
+
+int Contact:: create_contact()
+{
+    std:: cin.ignore();
+    std:: cout << "first-name: ";
+    std:: getline(std:: cin, first_name);
+    std:: cout << "last-name: ";
+    std:: getline(std:: cin, last_name);
+    std:: cout << "nickname: ";
+    std:: getline(std:: cin, nickname);
+    std:: cout << "darkest secret: ";
+    std:: getline(std:: cin, darkest_secret);
+    std:: cout << "number: ";
+    std:: getline(std:: cin, number);
+    return (!first_name.empty() && !last_name.empty() && !nickname.empty()\
+    && !darkest_secret.empty() && !number.empty());
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+int main(void)
+{
+    PhoneBook phonebook;
+    phonebook.size = 0;
+    std:: string input;
+    std:: cout << "Welcome to your PhoneBook :)\n";
+    std:: cout << "Type : ADD (to add a contact.)\n";
+    std:: cout << "Type : SEARCH (to search a contact.)\n";
+    std:: cout << "Type : EXIT (to quit the program.)\n";
+    do
+    {
+        std:: cout << ">>> ";
+        std:: cin >> input;
+        if(input.compare("ADD") == 0)
+          phonebook.add_contact();
+        else if (input.compare("SEARCH") == 0)
+            phonebook.search_contact();
+    }while (input.compare("EXIT"));
+    return 0;
+}
