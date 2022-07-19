a55d2313ee30f4fcd2d4593b20a6db8c9ed7c9d5
Fathiyat olatokunbo Jamia <fathjami@e3r13p3.1337.ma>
2022-07-19 17:01:48 +0100

    pointer vs reference

---
+++ CPP-Module-1/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void){
+
+    std::string str = "HI THIS IS BRAIN";
+    std::string *ptr = &str;
+    std::string &ref = str;
+
+    std::cout << &str << " : " << str << std::endl;
+    std::cout << ptr << " : " << *ptr << std::endl;
+    std::cout << &ref << " : " << ref << std::endl;
+    return 0;
+}
